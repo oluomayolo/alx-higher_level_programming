4e6946a35c7d2ec91c2c3700b70ef1c599734071
oluomayolo <oluomaokechukwu2020@gmail.com>
2023-03-08 15:18:39 -0800

    alx if else

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
+FIZZ = "Fizz"
+BUZZ = "Buzz"
+
+
 def fizzbuzz():
-    for i in range(1, 101):
-        if i % 3 == 0 and i % 5 == 0:
-            print("FizzBuzz", end='')
-        elif i % 3 == 0:
-            print("Fizz", end='')
-        elif i % 5 == 0:
-            print("Buzz", end='')
+    for number in range(1, 101):
+        if (number % 3 and number % 5):
+            print("%s%s" % (FIZZ, BUZZ), end=' ')
+        elif (number % 3):
+            print("%s" % (FIZZ), end=' ')
+        elif (number % 5):
+            print("%s" % (BUZZ), end=' ')
         else:
-            print(i, end='')
-
-        print(" ", end='')
-
+            print("%d" % (number), end=' ')
