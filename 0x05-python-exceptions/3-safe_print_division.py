6c6ea1fdda5b555567dd6740babe94020bf43c4a
oluomayolo <oluomaokechukwu2020@gmail.com>
2023-03-29 18:54:40 -0700

    exception task

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
+
     try:
-        result = a / b
-    except:
-        result = None
+        response = a / b
+    except ZeroDivisionError:
+        response = None
     finally:
-        print("Inside result: {}".format(result))
-        return (result)
+        print('Inside result: {}'.format(response))
+
+    return response
+
--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,20 +1,18 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
-    new_list = []
-    for i in range(list_length):
+    divisions = []
+    for index in range(list_length):
+        result = 0
         try:
-            result = my_list_1[i] / my_list_2[i]
+            result = my_list_1[index] / my_list_2[index]
         except ZeroDivisionError:
-            print("division by 0")
-            result = 0
+            print('division by 0')
         except TypeError:
-            print("wrong type")
-            result = 0
+            print('wrong type')
         except IndexError:
-            print("out of range")
-            result = 0
+            print('out of range')
         finally:
-            new_list.append(result)
+            divisions.append(result)
 
-    return (new_list)
+    return divisions
 
